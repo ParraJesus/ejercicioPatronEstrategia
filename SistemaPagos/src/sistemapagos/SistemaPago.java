b3f7ff9741e81a9d2045b2faed8936d63e3ed9fc
ParraJesus <jgparra@unicauca.edu.co>
2023-10-05 10:20:14 -0500

    v3

---
+++ SistemaPagos/src/sistemapagos/SistemaPago.java
@@ -0,0 +1,20 @@
+package sistemapagos;
+
+/**
+ *
+ * @author Jesus
+ */
+public class SistemaPago 
+{
+    private IPagoProcesable formaPago;
+    
+    public SistemaPago(IPagoProcesable formaPago)
+    {
+        this.formaPago = formaPago;
+    }
+    
+    public void realizarPago(double monto)
+    {
+        formaPago.procesarPago(monto);
+    }
+}
